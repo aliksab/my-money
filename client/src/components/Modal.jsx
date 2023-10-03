ea7a15e7f17889627843c58dd90ab575bc965053
aliksab <alexeysavich1112@gmail.com>
2023-10-03 13:49:24 +0900

    fix modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -45,24 +45,19 @@ export default function MyModal({title, icon, children}) {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-gray-800 p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Payment successful
-                  </Dialog.Title>
+
                   <div className="mt-2">
                     {children}
                   </div>
 
                   <div className="mt-4">
-                    <button
+                    {/* <button
                       type="button"
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
                       Got it, thanks!
-                    </button>
+                    </button> */}
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
